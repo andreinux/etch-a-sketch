66b1bf7357024393ce9e41244f11e38f0684ed5b
unknown <andreiacojido@gmail.com>
2026-06-18 22:42:28 +0800

    added functionality of the new page button for user input of desired number of boxes

--- main.js
+++ main.js
@@ -4,16 +4,21 @@ const newBtn = document.querySelector("#new-btn");
 
 // default sketch page
 
-let squaresNum = 256;
+let sideNumber = 16;
+let squareNumber = 16 * 16;
 
 function createGrid (input) {
 for (let i=0 ; i < input; i++) {
     let square = document.createElement("div");
     square.classList.add("square");
+    square.style.width=`calc(100% / ${sideNumber})`
+    square.style.height=`calc(100% / ${sideNumber})`
     fragment.append(square);
 }
+
+
 }
-createGrid(squaresNum);
+createGrid(squareNumber);
 container.append(fragment);
 
 //new sketch page
@@ -21,9 +26,9 @@ container.append(fragment);
 
 newBtn.addEventListener("click" , () => {
     sideNumber = parseInt(prompt("Enter the desired number of squares each side to make new page"));
-    squaresNum = sideNumber * sideNumber;
+    squareNumber = sideNumber * sideNumber;
     container.textContent = "";
-    createGrid(squaresNum);
+    createGrid(squareNumber);
     container.append(fragment);
 })
 
