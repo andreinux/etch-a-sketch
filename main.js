807d29a8db42ed48408dd21bf255ce0c102c48ea
unknown <andreiacojido@gmail.com>
2026-06-18 14:15:58 +0800

    added event handler for new page button that removes the default page and applies the user's desired number of sqaures-this still needs ficing with css

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 const container = document.querySelector("#container");
-
 const fragment = document.createDocumentFragment();
+const newBtn = document.querySelector("#new-btn");
+
+// default sketch page
 
 let squaresNum = 256;
 
-for (let i=0 ; i < squaresNum; i++) {
+function createGrid (input) {
+for (let i=0 ; i < input; i++) {
     let square = document.createElement("div");
     square.classList.add("square");
     fragment.append(square);
 }
-
+}
+createGrid(squaresNum);
 container.append(fragment);
+
+//new sketch page
+
+
+newBtn.addEventListener("click" , () => {
+    sideNumber = parseInt(prompt("Enter the desired number of squares each side to make new page"));
+    squaresNum = sideNumber * sideNumber;
+    container.textContent = "";
+    createGrid(squaresNum);
+    container.append(fragment);
+})
+
