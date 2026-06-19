9d1d12c7849a490bf3619eb7320bb16bcd389cbc
unknown <andreiacojido@gmail.com>
2026-06-19 12:36:03 +0800

    improved color tracking by activating it only with mousedown, also added the final touches

--- main.js
+++ main.js
@@ -7,6 +7,21 @@ const erase = document.querySelector("#erase");
 let sideNumber = 16;
 let squareNumber = 16 * 16;
 
+
+
+let isDrawing = false;
+
+document.addEventListener("mousedown" , (e) => {
+    if (e.button===0) {
+    isDrawing= true;
+    }
+});
+document.addEventListener("mouseup" , () => {
+     isDrawing = false;
+});
+
+
+
 function createGrid (input) {
 for (let i=0 ; i < input; i++) {
     let square = document.createElement("div");
@@ -14,18 +29,33 @@ for (let i=0 ; i < input; i++) {
     square.style.width=`calc(100% / ${sideNumber})`
     square.style.height=`calc(100% / ${sideNumber})`
 
-    square.addEventListener("mouseover" , () => {
-        square.style.backgroundColor="red";
-    })
-
-
+    
     fragment.append(square);
-}
+
+square.addEventListener("mousedown" , () => {
+        square.style.backgroundColor="red";
+});
+    
+square.addEventListener("mouseover" , () => {
+    if (isDrawing) {
+        square.style.backgroundColor="red";
+    }
+});
+
+}};
 
 
-}
 createGrid(squareNumber);
 container.append(fragment);
+
+
+
+
+
+
+
+
+
 
 //new sketch page
 
@@ -34,8 +64,11 @@ newBtn.addEventListener("click" , () => {
     sideNumber = parseInt(prompt("Enter the desired number of squares each side to make new page"));
 
 
-    if (sideNumber >= 101) {
+    if (sideNumber < 1 || sideNumber > 100) {
         alert("The sketchpad is only limited to 100 squares per side");
+    } 
+    else if (isNaN(sideNumber)) {
+        alert("Please Input a Valid Number of Sides");
     }
   else {
     squareNumber = sideNumber * sideNumber;
